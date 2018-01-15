0dc33a626c6e08785eb25a95bbc14bfd4ee5de2f
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2018-01-15 12:56:14 +0000

    refactored for loops

--- src/encode.js
+++ src/encode.js
@@ -8,7 +8,7 @@ function encode(string) {
   for(let i = 0 ; i < string.length; i++){if(string[i] !== string[i+1]){letter.push(string[i]);number.push(i+1)}};
   count.push(number[0]);
   for(let i = 1; i < letter.length; i++){count.push(number[i] - number[i-1])};
-  for(let i = 0; i < letter.length; i++){result.push(count[i], letter[i])};
-  for(let i = 0; i < result.length; i++){if(result[i] === 1){result.splice(i,1)}}
+  count.forEach((item, i) => {result.push(count[i], letter[i])});
+  result.forEach((item, i) => {if (item === 1){result.splice(i,1)}});
   return result.join("");
 }
